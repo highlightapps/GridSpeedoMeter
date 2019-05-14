4929df3def7dba90ec369bae4a2064bc91347f39
highlightapps@gmail.com <suryaprakash.kaipa@serviceaide.com>
2019-05-14 10:02:34 +0530

    contact ui is started

--- app/src/main/java/com/example/viewpagerpoc/PhoneContactsFragment.java
+++ app/src/main/java/com/example/viewpagerpoc/PhoneContactsFragment.java
@@ -12,11 +12,13 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.android.vcard.*;
+import com.android.vcard.VCardEntry;
 
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -45,12 +47,26 @@ public class PhoneContactsFragment extends BaseFragment {
     boolean retrieve_phone_contact = false;
     boolean retrieve_sim_contact = false;
 
+
+    //Views
+    private RecyclerView recyclerView;
+    PhoneFragmentContactsAdapter phoneFragmentContactsAdapter;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_contacts, container, false);
-        //initViews(view);
         initLogic();
+        initViews(view);
         return view;
+    }
+
+    private void initViews(View view) {
+        recyclerView = view.findViewById(R.id.my_recycler_view);
+        recyclerView.setHasFixedSize(true);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layoutManager);
+        phoneFragmentContactsAdapter = new PhoneFragmentContactsAdapter(contacts);
+        recyclerView.setAdapter(phoneFragmentContactsAdapter);
     }
 
     private void initLogic(){
@@ -186,6 +202,10 @@ public class PhoneContactsFragment extends BaseFragment {
                 if (((ArrayList) msg.obj) != null) {
                     contacts.addAll((ArrayList) msg.obj);
                 }
+                if(contacts != null && !contacts.isEmpty()) {
+                    phoneFragmentContactsAdapter.setContacts(contacts);
+                    phoneFragmentContactsAdapter.notifyDataSetChanged();
+                }
                 sPbapClient.pullPhoneBook(BluetoothPbapClient.SIM_PB_PATH);
             } else if (!retrieve_sim_contact) {
                 //hideProgressDialog();
@@ -209,6 +229,11 @@ public class PhoneContactsFragment extends BaseFragment {
                 retrieve_phone_contact = false;
                 retrieve_sim_contact = false;
                 contacts.clear();
+            }
+
+            if(contacts != null && !contacts.isEmpty()) {
+                phoneFragmentContactsAdapter.setContacts(contacts);
+                phoneFragmentContactsAdapter.notifyDataSetChanged();
             }
         }
     }
@@ -236,38 +261,4 @@ public class PhoneContactsFragment extends BaseFragment {
             }
         }
     }
-
-
-
-    /*private void initViews(View view) {
-
-        Intent serviceIntent = new Intent(getContext(), BluetoothService.class);
-        ServiceConnection connection = new ServiceConnection() {
-            @Override
-            public void onServiceConnected(ComponentName name, IBinder service) {
-                Log.d("", "onServiceConnected, init the sService");
-                sService = ((BluetoothService.BluetoothServiceBinder) service).getService();
-            }
-
-            @Override
-            public void onServiceDisconnected(ComponentName name) {
-                sService = null;
-            }
-        };
-        getActivity().bindService(serviceIntent, connection, Context.BIND_AUTO_CREATE);
-
-        Button getContacts = view.findViewById(R.id.getContacts);
-        getContacts.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onPullPhoneBook();
-            }
-        });
-    }
-
-    public void onPullPhoneBook() {
-        if (sService != null) {
-            sService.getPhoneBook();
-        }
-    }*/
 }
---
+++ app/src/main/java/com/example/viewpagerpoc/PhoneFragmentContactsAdapter.java
@@ -0,0 +1,51 @@
+package com.example.viewpagerpoc;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.android.vcard.VCardEntry;
+
+import java.util.ArrayList;
+
+public class PhoneFragmentContactsAdapter extends RecyclerView.Adapter<PhoneFragmentContactsAdapter.ContactsViewHolder> {
+
+    ArrayList<VCardEntry> contacts;
+
+    public PhoneFragmentContactsAdapter(ArrayList<VCardEntry> contacts){
+        this.contacts = contacts;
+    }
+
+    @NonNull
+    @Override
+    public ContactsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.phone_contacts_list_item, parent, false);
+        ContactsViewHolder myViewHolder = new ContactsViewHolder(view);
+        return myViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ContactsViewHolder myViewHolder, int position) {
+        myViewHolder.textViewContactName.setText(contacts.get(position).getDisplayName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return contacts != null ? contacts.size() : 0;
+    }
+
+    public void setContacts(ArrayList<VCardEntry> contacts){
+        this.contacts = contacts;
+    }
+
+    public static class ContactsViewHolder extends RecyclerView.ViewHolder {
+        public TextView textViewContactName;
+        public ContactsViewHolder(View v) {
+            super(v);
+            textViewContactName = v.findViewById(R.id.tvContactName);
+        }
+    }
+}
