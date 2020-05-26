d0c771830fdb8e213cb1249dbcc37b34699313aa
Surya Prakash Kaipa <suryaprakash.kaipa@serviceaide.com>
2020-05-26 21:26:03 +0530

    pdf generation

---
+++ pdfwriter/src/main/java/com/jasper/pdfcreator/App.java
@@ -0,0 +1,13 @@
+package com.jasper.pdfcreator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ pdfwriter/src/main/java/com/jasper/pdfcreator/GenerateReportService.java
@@ -0,0 +1,71 @@
+package com.jasper.pdfcreator;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.data.JsonDataSource;
+import net.sf.jasperreports.engine.design.JasperDesign;
+import net.sf.jasperreports.engine.export.JRPdfExporter;
+import net.sf.jasperreports.engine.xml.JRXmlLoader;
+import net.sf.jasperreports.export.SimpleExporterInput;
+import net.sf.jasperreports.export.SimpleOutputStreamExporterOutput;
+
+public class GenerateReportService {
+
+	public static void main(String[] args) {
+		generatePdfReport();
+	}
+
+	// This method generates a PDF report
+	public static void generatePdfReport() {
+		try {
+			File initialFile = new File("src/main/resources/data.jrxml");
+			InputStream jrxmlInput = new FileInputStream(initialFile);
+			JasperDesign design = JRXmlLoader.load(jrxmlInput);
+			JasperReport jasperReport = JasperCompileManager.compileReport(design);
+			String data = readDataFromFile();
+			// It is possible to generate Jasper reports from a JSON source.
+			JsonDataSource jsonDataSource = new JsonDataSource(new ByteArrayInputStream(data.getBytes()));
+			JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, new HashMap(), jsonDataSource);
+
+			JRPdfExporter pdfExporter = new JRPdfExporter();
+			pdfExporter.setExporterInput(new SimpleExporterInput(jasperPrint));
+			ByteArrayOutputStream pdfReportStream = new ByteArrayOutputStream();
+			pdfExporter.setExporterOutput(new SimpleOutputStreamExporterOutput(pdfReportStream));
+			pdfExporter.exportReport();
+
+			FileOutputStream fout1 = new FileOutputStream("src/main/resources/data1.pdf");
+			pdfReportStream.writeTo(fout1);
+			pdfReportStream.flush();
+			pdfReportStream.close();
+		} catch (Exception e) {
+			System.out.println("@@@ Error Message @@@ :" + e.getMessage());
+		}
+	}
+
+	private static String readDataFromFile() throws Exception {
+		InputStream is = new FileInputStream("src/main/resources/data.json");
+		BufferedReader buf = new BufferedReader(new InputStreamReader(is));
+
+		String line = buf.readLine();
+		StringBuilder sb = new StringBuilder();
+
+		while (line != null) {
+			sb.append(line).append("\n");
+			line = buf.readLine();
+		}
+
+		return sb.toString();
+	}
+}
